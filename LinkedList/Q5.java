26a3304b8a72c42f2fb481b0d5efe67015389cee
garv-cmd <54171077+garv-cmd@users.noreply.github.com>
2020-08-05 23:09:17 +0530

    SDE Interview Coding Questions

---
+++ LinkedList/Q5.java
@@ -0,0 +1,14 @@
+package LinkedList;
+//Delete a given Node when a node is given.
+//LEETCODE -> 237
+public class Q5 {
+    /*
+    *
+     public void deleteNode(ListNode node) {
+      node.val = node.next.val;
+        node.next = node.next.next;
+
+    }
+    * */
+
+}
