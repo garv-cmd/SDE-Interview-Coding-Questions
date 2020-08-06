1c5f72a1e76bbe2ea5d4c29bced471760bb86f49
garv-cmd <54171077+garv-cmd@users.noreply.github.com>
2020-08-06 22:21:36 +0530

    SDE Interview Coding Questions

---
+++ LinkedList/Q7.java
@@ -0,0 +1,5 @@
+package LinkedList;
+//Find intersection point of Y LinkedList
+
+public class Q7 {
+}
---
+++ LinkedList/Q8.java
@@ -0,0 +1,23 @@
+package LinkedList;
+//Check if a LinkedList is palindrome or not
+//LEETCODE -> 234
+public class Q8 {
+    /*public boolean isPalindrome(ListNode head) {
+        if(head==null)
+            return true;
+        Stack<Integer> stack = new Stack<>();
+        ListNode temp = head;
+        while(temp!=null){
+            stack.push(temp.val);
+            temp = temp.next;
+        }
+        while(!stack.isEmpty()){
+            if(head.val!=stack.pop()){
+                return false;
+            }
+            head = head.next;
+
+        }
+        return true;
+    }*/
+}
