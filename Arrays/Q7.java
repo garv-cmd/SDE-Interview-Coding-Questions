fde0ee45694b839fc6fef2b711f79fb540cdbf04
garv-cmd <54171077+garv-cmd@users.noreply.github.com>
2020-08-13 22:14:59 +0530

    SDE Interview Coding Questions

---
+++ Arrays/Q7.java
@@ -0,0 +1,39 @@
+package Arrays;
+//LEETCODE -> 73
+public class Q7 {
+    /*class Solution {
+    public void setZeroes(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int r[] = new int [row];
+        int c[] = new int[col];
+        System.out.println(row);
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(matrix[i][j]==0){
+                    r[i] = 1;
+                    c[j] = 1;
+                }
+            }
+        }
+        for(int i:c)
+            System.out.println(i);
+        for(int i=0;i<c.length;i++){
+            if(c[i]==1){
+                for(int j=0;j<row;j++){
+                    matrix[j][i]=0;
+                }
+            }
+        }
+        for(int i=0;i<r.length;i++){
+            if(r[i]==1){
+                for(int j=0;j<col;j++){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+
+
+    }
+}*/
+}
---
+++ LinkedList/Q9.java
@@ -0,0 +1,4 @@
+package LinkedList;
+//Reverse a LinkedList in groups.
+public class Q9 {
+}
